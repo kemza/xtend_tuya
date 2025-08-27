aa4bc00b582a0f3142d07625cac819e4e2c25de2
Lubor Kemza <1448884+kemza@users.noreply.github.com>
2025-08-27 08:43:06 +0200

    new file:   custom_components/mower.py

---
+++ custom_components/mower.py
@@ -0,0 +1,75 @@
+"""Support for Tuya Mowers."""
+
+from __future__ import annotations
+from typing import cast
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .multi_manager.multi_manager import (
+    MultiManager,
+    XTConfigEntry,
+    XTDevice,
+)
+from .const import TUYA_DISCOVERY_NEW
+from .entity import (
+    XTEntity,
+    XTEntityDescriptorManager,
+)
+from .ha_tuya_integration.tuya_integration_imports import (
+    TuyaMowerEntity,
+)
+
+MOWERS: list[str] = []
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: XTConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Set up Tuya mower dynamically through Tuya discovery."""
+    hass_data = entry.runtime_data
+
+    if entry.runtime_data.multi_manager is None or hass_data.manager is None:
+        return
+
+    supported_descriptors, externally_managed_descriptors = cast(
+        tuple[
+            list[str],
+            list[str],
+        ],
+        XTEntityDescriptorManager.get_platform_descriptors(
+            MOWERS, entry.runtime_data.multi_manager, Platform.MOWER
+        ),
+    )
+
+    @callback
+    def async_discover_device(device_map, restrict_dpcode: str | None = None) -> None:
+        """Discover and add a discovered Tuya mower."""
+        if hass_data.manager is None:
+            return
+        if restrict_dpcode is not None:
+            return None
+        entities: list[XTMowerEntity] = []
+        device_ids = [*device_map]
+        for device_id in device_ids:
+            if device := hass_data.manager.device_map.get(device_id):
+                if device.category in supported_descriptors:
+                    entities.append(XTMowerEntity(device, hass_data.manager))
+        async_add_entities(entities)
+
+    async_discover_device([*hass_data.manager.device_map])
+
+    entry.async_on_unload(
+        async_dispatcher_connect(hass, TUYA_DISCOVERY_NEW, async_discover_device)
+    )
+
+
+class XTMowerEntity(XTEntity, TuyaMowerEntity):
+    """XT Mower Device."""
+
+    def __init__(self, device: XTDevice, device_manager: MultiManager) -> None:
+        """Init Tuya mower."""
+        super(XTMowerEntity, self).__init__(device, device_manager)
+        super(XTEntity, self).__init__(device, device_manager)  # type: ignore
+        self.device = device
+        self.device_manager = device_manager
